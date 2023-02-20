029fb0fb5b2403ee9c336910c108d3fd219f1838
Lawrinda-git <lawrindahobokwaah@gmail.com>
2023-02-20 14:21:40 -0800

    Hellooo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - the main function
+ * main - This is the main function
  *
  * Return: 0
  */
@@ -9,5 +9,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);	
+	return (0);
 }
