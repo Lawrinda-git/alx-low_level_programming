a463930bb125474a01442434aab93a0322fd3a2a
Lawrinda-git <lawrindahobokwaah@gmail.com>
2023-02-20 15:42:18 -0800

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main- This is the starting point for program execution
  *
- * Return: 0
+ * Return: 0 when successful
  */
 
 int main(void)
@@ -11,7 +11,7 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int : %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)", sizeof(float));
 	return (0);
 }
