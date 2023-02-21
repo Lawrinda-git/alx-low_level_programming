cf3a8640a68291a101d818f897fb02fd5afd785d
Lawrinda-git <lawrindahobokwaah@gmail.com>
2023-02-20 16:34:06 -0800

    intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main-This is the starting point to execute program
+ *
+ * Return: 1 when successful
+ */
+
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}	
