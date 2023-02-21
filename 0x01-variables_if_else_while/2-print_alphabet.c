e65024231b147939e84e2454b94d54ec308161cb
Lawrinda-git <lawrindahobokwaah@gmail.com>
2023-02-20 18:40:13 -0800

    lowercase alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
