d3634e219ed5ef2adc10ccde43e3dce3b85dd85a
Lawrinda-git <lawrindahobokwaah@gmail.com>
2023-02-20 19:17:30 -0800

    Some things needs to be cut off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point for program execution
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
